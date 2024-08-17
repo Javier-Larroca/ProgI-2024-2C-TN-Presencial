437e4d87751338f8bb883ae4e961d20a7cb1f2d2
Javier Larroca <javier-larroca@hotmail.com>
2024-08-16 23:15:31 -0300

    Archivo de presentacion de condicionales y primer ejercicio de guia 2

---
+++ Clase 02/g02_ej01/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+/*
+ 1 - Hacer un programa para ingresar por teclado un número y luego emitir por
+ pantalla un cartel aclaratorio indicando si el mismo es positivo, negativo o cero.
+*/
+
+int main()
+{
+    int n;
+
+    cout << "Ingrese un numero: ";
+    cin >> n;
+
+    if(n>0)
+    {
+        cout << "El Numero " << n << " es Positivo." << endl;
+    }
+    else if(n==0)
+    {
+        cout << "El Numero " << n << " es Cero." << endl;
+    }
+    else
+    {
+        cout << "El Numero " << n << " es Negativo." << endl;
+    }
+    return 0;
+}
